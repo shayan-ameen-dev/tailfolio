aefd5bdb68e18a219fd7d4c5adc8719ed294b2d5
shayan <shayan.ameen.dev@gmail.com>
2022-10-03 10:12:54 +0500

    adding tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>Next App</h1>
+        <h1 className="text-3xl font-bold underline">Next App</h1>{' '}
       </main>
     </div>
   );
